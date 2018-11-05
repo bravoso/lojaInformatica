b64bc77909aca73bac5d6eb517a84fc13ddedbb5
Lucas Willuweit <Lucas Willuweit@DESKTOP-T15MK7T>
2018-11-05 20:13:59 -0200

    Atualizadas as interfaces de produtos, clientes e serviços.

--- LojaInformatica/src/br/senac/dd/cyberimports/view/Principal.java
+++ LojaInformatica/src/br/senac/dd/cyberimports/view/Principal.java
@@ -20,10 +20,33 @@ import java.awt.Component;
 import javax.swing.Box;
 import javax.swing.SpringLayout;
 import java.awt.Dimension;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
+import javax.swing.JCheckBox;
 
 public class Principal extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtNome;
+	private JTextField txtCPF;
+	private JTextField txtEndereco;
+	private JTextField txtTelefone;
+	private JTable tableClientes;
+	private JTextField textField;
+	private JTable table;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField txtIdProduto;
+	private JTextField txtNomeProduto;
+	private JTextField txtPrecoCusto;
+	private JTextField txtPrecoVenda;
+	private JTextField txtQuantidade;
+	private JTable table_1;
 
 	/**
 	 * Launch the application.
@@ -49,7 +72,7 @@ public class Principal extends JFrame {
 		setResizable(false);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1200, 700);
+		setBounds(100, 100, 870, 707);
 
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -182,28 +205,199 @@ public class Principal extends JFrame {
 		// CRIAÇÃO DAS ABAS DO SISTEMA.
 
 		JPanel pnClientes = new JPanel();
+		
+				JPanel pnProdutos = new JPanel();
+				tpAbas.addTab("Produtos", iconeProdutos, pnProdutos, null);
+				pnProdutos.setLayout(null);
+				
+				JLabel lblNomeDoProduto = new JLabel("Nome do Produto:");
+				lblNomeDoProduto.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblNomeDoProduto.setBounds(10, 45, 123, 14);
+				pnProdutos.add(lblNomeDoProduto);
+				
+				JLabel lblPreoDeCusto = new JLabel("Pre\u00E7o de custo:");
+				lblPreoDeCusto.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblPreoDeCusto.setBounds(10, 81, 123, 14);
+				pnProdutos.add(lblPreoDeCusto);
+				
+				JLabel lblPreoDeVenda = new JLabel("Pre\u00E7o de venda:");
+				lblPreoDeVenda.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblPreoDeVenda.setBounds(10, 116, 123, 14);
+				pnProdutos.add(lblPreoDeVenda);
+				
+				JLabel lblIdDoProduto = new JLabel("ID do Produto:");
+				lblIdDoProduto.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblIdDoProduto.setBounds(10, 11, 123, 14);
+				pnProdutos.add(lblIdDoProduto);
+				
+				JLabel lblQuantidade = new JLabel("Quantidade:");
+				lblQuantidade.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblQuantidade.setBounds(10, 149, 123, 14);
+				pnProdutos.add(lblQuantidade);
+				
+				txtIdProduto = new JTextField();
+				txtIdProduto.setBounds(132, 8, 86, 20);
+				pnProdutos.add(txtIdProduto);
+				txtIdProduto.setColumns(10);
+				
+				txtNomeProduto = new JTextField();
+				txtNomeProduto.setColumns(10);
+				txtNomeProduto.setBounds(132, 42, 175, 20);
+				pnProdutos.add(txtNomeProduto);
+				
+				txtPrecoCusto = new JTextField();
+				txtPrecoCusto.setColumns(10);
+				txtPrecoCusto.setBounds(132, 78, 86, 20);
+				pnProdutos.add(txtPrecoCusto);
+				
+				txtPrecoVenda = new JTextField();
+				txtPrecoVenda.setColumns(10);
+				txtPrecoVenda.setBounds(132, 113, 86, 20);
+				pnProdutos.add(txtPrecoVenda);
+				
+				txtQuantidade = new JTextField();
+				txtQuantidade.setColumns(10);
+				txtQuantidade.setBounds(132, 146, 86, 20);
+				pnProdutos.add(txtQuantidade);
+				
+				table_1 = new JTable();
+				table_1.setBounds(320, 11, 522, 524);
+				pnProdutos.add(table_1);
+				
+				JButton btnSalvarProduto = new JButton("Salvar");
+				btnSalvarProduto.setBounds(10, 223, 89, 23);
+				pnProdutos.add(btnSalvarProduto);
+				
+				JButton btnExcluirProduto = new JButton("Excluir");
+				btnExcluirProduto.setBounds(218, 223, 89, 23);
+				pnProdutos.add(btnExcluirProduto);
+				
+				JCheckBox chckbxRemover = new JCheckBox("Remover");
+				chckbxRemover.setFont(new Font("Tahoma", Font.BOLD, 11));
+				chckbxRemover.setBounds(10, 193, 89, 23);
+				pnProdutos.add(chckbxRemover);
+				
+				JCheckBox chckbxAdicionar = new JCheckBox("Adicionar");
+				chckbxAdicionar.setFont(new Font("Tahoma", Font.BOLD, 11));
+				chckbxAdicionar.setBounds(10, 170, 123, 23);
+				pnProdutos.add(chckbxAdicionar);
 
 		JPanel servico = new JPanel();
 		tpAbas.addTab("Orçamentos", iconeOrcamento, servico, null);
-
-		JPanel pnProdutos = new JPanel();
-		tpAbas.addTab("Produtos", iconeProdutos, pnProdutos, null);
-
-		JPanel pnServico = new JPanel();
-		tpAbas.addTab("Serviços", iconeServicos, pnServico, null);
+		
+				JPanel pnServico = new JPanel();
+				tpAbas.addTab("Serviços", iconeServicos, pnServico, null);
+				pnServico.setLayout(null);
+				
+				JLabel lblNomeDoServio = new JLabel("Nome do Servi\u00E7o:");
+				lblNomeDoServio.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblNomeDoServio.setBounds(25, 54, 130, 14);
+				pnServico.add(lblNomeDoServio);
+				
+				textField = new JTextField();
+				textField.setBounds(155, 51, 226, 20);
+				pnServico.add(textField);
+				textField.setColumns(10);
+				
+				table = new JTable();
+				table.setBounds(25, 171, 818, 364);
+				pnServico.add(table);
+				
+				JButton btnAdicionar = new JButton("Adicionar");
+				btnAdicionar.setBounds(25, 126, 89, 23);
+				pnServico.add(btnAdicionar);
+				
+				JButton btnSalvar_1 = new JButton("Salvar");
+				btnSalvar_1.setBounds(155, 126, 89, 23);
+				pnServico.add(btnSalvar_1);
+				
+				JButton btnExcluir_1 = new JButton("Excluir");
+				btnExcluir_1.setBounds(279, 126, 89, 23);
+				pnServico.add(btnExcluir_1);
+				
+				textField_1 = new JTextField();
+				textField_1.setColumns(10);
+				textField_1.setBounds(155, 80, 114, 20);
+				pnServico.add(textField_1);
+				
+				JLabel label = new JLabel("Valor do Servi\u00E7o:");
+				label.setFont(new Font("Tahoma", Font.BOLD, 11));
+				label.setBounds(25, 83, 130, 14);
+				pnServico.add(label);
+				
+				textField_2 = new JTextField();
+				textField_2.setColumns(10);
+				textField_2.setBounds(155, 23, 114, 20);
+				pnServico.add(textField_2);
+				
+				JLabel lblIdDoServio = new JLabel("ID do Servi\u00E7o:");
+				lblIdDoServio.setFont(new Font("Tahoma", Font.BOLD, 11));
+				lblIdDoServio.setBounds(25, 26, 130, 14);
+				pnServico.add(lblIdDoServio);
 		tpAbas.addTab("Clientes", iconeClientes, pnClientes, null);
 		pnClientes.setLayout(null);
-
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(10, 11, 1144, 538);
-		pnClientes.add(tabbedPane);
-
-		JPanel pnClientesCadastro = new JPanel();
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1154, 535);
+		pnClientes.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNome.setBounds(22, 93, 83, 14);
+		panel.add(lblNome);
+		
+		txtNome = new JTextField();
+		txtNome.setBounds(115, 93, 277, 20);
+		panel.add(txtNome);
+		txtNome.setColumns(10);
+		
+		JLabel lblCpf = new JLabel("CPF:");
+		lblCpf.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblCpf.setBounds(22, 124, 83, 14);
+		panel.add(lblCpf);
+		
+		txtCPF = new JTextField();
+		txtCPF.setColumns(10);
+		txtCPF.setBounds(115, 124, 277, 20);
+		panel.add(txtCPF);
+		
+		JLabel lblEndereo = new JLabel("Endere\u00E7o:");
+		lblEndereo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblEndereo.setBounds(22, 161, 83, 14);
+		panel.add(lblEndereo);
+		
+		txtEndereco = new JTextField();
+		txtEndereco.setColumns(10);
+		txtEndereco.setBounds(115, 161, 277, 20);
+		panel.add(txtEndereco);
+		
+		JLabel lblTelefone = new JLabel("Telefone:");
+		lblTelefone.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblTelefone.setBounds(22, 197, 83, 14);
+		panel.add(lblTelefone);
+		
+		txtTelefone = new JTextField();
+		txtTelefone.setColumns(10);
+		txtTelefone.setBounds(115, 197, 277, 20);
+		panel.add(txtTelefone);
+		
+		tableClientes = new JTable();
+		tableClientes.setBounds(402, 37, 435, 487);
+		panel.add(tableClientes);
+		
+		JButton btnSalvar = new JButton("Salvar");
+		btnSalvar.setBounds(22, 299, 107, 23);
+		panel.add(btnSalvar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBounds(303, 299, 89, 23);
+		panel.add(btnExcluir);
+		
+		JButton btnProcurar = new JButton("Procurar");
+		btnProcurar.setBounds(170, 299, 89, 23);
+		panel.add(btnProcurar);
 		ImageIcon iconeAddCliente = new ImageIcon(Principal.class.getResource("/icons/icons8-add-48.png"));
-		tabbedPane.addTab("Cadastrar", iconeAddCliente, pnClientesCadastro, null);
-
-		JPanel pnClienteLista = new JPanel();
-		tabbedPane.addTab("Listar Clientes", iconeClienteLista, pnClienteLista, null);
 
 	}
 }
