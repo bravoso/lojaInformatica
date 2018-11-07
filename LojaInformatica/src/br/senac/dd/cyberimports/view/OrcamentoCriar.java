ccb9dfbf43d9da550aac4e49d96e3a454f6f86b3
Lucas Willuweit <Lucas Willuweit@DESKTOP-T15MK7T>
2018-11-07 19:50:11 -0200

    Criada a tela OrçamentoCriar

--- LojaInformatica/src/br/senac/dd/cyberimports/view/OrcamentoCriar.java
+++ LojaInformatica/src/br/senac/dd/cyberimports/view/OrcamentoCriar.java
@@ -5,17 +5,32 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.Font;
 
 public class OrcamentoCriar {
 
-	private JFrame frame;
+	private JFrame frmNovoOramento;
+	private JTable table;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	public JFrame getFrame() {
-		return frame;
+		return frmNovoOramento;
 	}
 
 	public void setFrame(JFrame frame) {
-		this.frame = frame;
+		this.frmNovoOramento = frame;
 	}
 
 	/**
@@ -29,10 +44,81 @@ public class OrcamentoCriar {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 540, 476);
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmNovoOramento = new JFrame();
+		frmNovoOramento.setIconImage(Toolkit.getDefaultToolkit().getImage(OrcamentoCriar.class.getResource("/icons/icons8-add-48.png")));
+		frmNovoOramento.setTitle("Novo Or\u00E7amento");
+		frmNovoOramento.setBounds(100, 100, 675, 375);
+		frmNovoOramento.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frmNovoOramento.getContentPane().setLayout(null);
+		
+		table = new JTable();
+		table.setBorder(UIManager.getBorder("ComboBox.border"));
+		table.setBounds(10, 167, 639, 158);
+		frmNovoOramento.getContentPane().add(table);
+		
+		JLabel lblCliente = new JLabel("Cliente:");
+		lblCliente.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblCliente.setBounds(10, 11, 63, 14);
+		frmNovoOramento.getContentPane().add(lblCliente);
+		
+		textField = new JTextField();
+		textField.setBounds(83, 8, 175, 20);
+		frmNovoOramento.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(123, 39, 135, 20);
+		frmNovoOramento.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblData = new JLabel("Data de Cria\u00E7\u00E3o:");
+		lblData.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblData.setBounds(10, 45, 103, 14);
+		frmNovoOramento.getContentPane().add(lblData);
+		
+		JLabel lblStatus = new JLabel("Status:");
+		lblStatus.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblStatus.setBounds(330, 11, 46, 14);
+		frmNovoOramento.getContentPane().add(lblStatus);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Aguardando", "Em Andamento", "Aguardando Pe\u00E7as", "Or\u00E7amento conclu\u00EDdo", "Finalizado"}));
+		comboBox.setBounds(401, 8, 120, 20);
+		frmNovoOramento.getContentPane().add(comboBox);
+		
+		JLabel lblVendedoratendente = new JLabel("Vendedor:");
+		lblVendedoratendente.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblVendedoratendente.setBounds(330, 45, 129, 14);
+		frmNovoOramento.getContentPane().add(lblVendedoratendente);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(401, 42, 120, 20);
+		frmNovoOramento.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblProcurarProdutoOu = new JLabel("Procurar produto ou Servi\u00E7o:");
+		lblProcurarProdutoOu.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblProcurarProdutoOu.setBounds(10, 142, 175, 14);
+		frmNovoOramento.getContentPane().add(lblProcurarProdutoOu);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(172, 139, 287, 20);
+		frmNovoOramento.getContentPane().add(textField_3);
+		textField_3.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Procurar");
+		btnNewButton.setIcon(new ImageIcon(OrcamentoCriar.class.getResource("/icons/icons8-google-web-search-24.png")));
+		btnNewButton.setBounds(473, 138, 113, 23);
+		frmNovoOramento.getContentPane().add(btnNewButton);
+		
+		JLabel lblProblemaDeclarado = new JLabel("Problema declarado:");
+		lblProblemaDeclarado.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblProblemaDeclarado.setBounds(10, 93, 129, 14);
+		frmNovoOramento.getContentPane().add(lblProblemaDeclarado);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(173, 70, 413, 61);
+		frmNovoOramento.getContentPane().add(textField_4);
+		textField_4.setColumns(10);
 	}
-
 }
